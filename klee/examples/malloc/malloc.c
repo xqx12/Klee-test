202e28e2f6e67cf99c65be3141a59cbc5351e581
ubuntu <xqx@server.(none)>
2014-03-15 09:42:33 +0800

    modify klee-gcc for building texinfo-4.8
    Signed-off-by: ubuntu <xqx@server.(none)>

---
+++ klee/examples/malloc/malloc.c
@@ -0,0 +1,63 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  malloc.c
+ *
+ *    Description:  test  malloc in klee
+ *
+ *        Version:  1.0
+ *        Created:  2014年02月12日 14时03分56秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Xiao Qixue (XQX), xiaoqixue_1@163.com
+ *   Organization:  Tsinghua University
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdlib.h>
+
+int doSomething(int x, void * buf)
+{
+   if (x%3 == 2)
+   {
+      free((void*)buf);
+      return 0;
+   }
+
+   return 1;
+}
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+	int
+main ( int argc, char *argv[] )
+{
+	int size;
+	int x;
+	
+	if ( argc < 3 )
+		return 0;
+
+	size = atoi( argv[1] );
+	x = atoi( argv[2] );
+
+	int * myBuf = (int *)malloc(sizeof(int)*size);
+	/*if (myBuf != NULL)*/
+		/*free((void *)myBuf);*/
+
+	if (!doSomething(x, myBuf))
+	{
+		free((void *)myBuf);
+	}
+
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
+
+
+
